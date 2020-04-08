ed87097339ef9d25a2ad28b5be539c29e9580c3e
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-08 16:44:52 +0200

    [Refactoring]Refactored the app structure. [FUNCTION] Added functionality to show an individual book

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const  booksController  = require('../controllers/bookController')
+const  booksController  = require('../controllers/booksController')
 
 router
 .get('/', booksController.index)

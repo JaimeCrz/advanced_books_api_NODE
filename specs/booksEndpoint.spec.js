18c7cfd4addf454a4ab86ca6fa2256771efd8cd2
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-08 16:14:41 +0200

    Adding test for finding book by id

--- specs/booksEndpoint.spec.js
+++ specs/booksEndpoint.spec.js
@@ -24,8 +24,25 @@ describe('GET /api/v1/books', () => {
   });
 
   it('responds with list of books', () => {
-    const expectedBody = {books: [{title: 'Death note'}] }
-    expect(jsonResponse(response))
-    .to.equal(JSON.stringify(expectedBody))
+    expect(response.body.books)
+    .to.be.an("array")
+  });
+
+  it('returns title for book', () => {
+    expect(response.body.books[1].title).to.equal('Learning Angular for dummies')
   });
 })
+
+describe('GET /api/v1/books/:id', () => {
+  before(async () => {
+    response = await request.get('/api/v1/books/1')
+  })
+
+  it('responds with status 200', () => {
+    expect(response.status).to.equal(200)
+  });
+
+  it('responds a single book', () => {
+    expect(response.body.book.id).to.equal(1)
+  });
+});

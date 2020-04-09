d3c5c9bbf871e728df15b1a9f67c098ac0150103
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-09 11:21:38 +0200

    [MODIFIED] added factories and new line to avoid deprecating errors

--- test/booksEndpoint.spec.js
+++ test/booksEndpoint.spec.js
@@ -2,17 +2,29 @@ const app = require('../app')
 const supertest = require('supertest')
 const expect = require('chai').expect
 const jsonResponse = require('./jsonResponse')
+const { factory, Models } = require('../test_helpers')
 
 let server, request, response
 
 before((done) => {
-    server = app.listen(done)
-    request = supertest.agent(server)
+  server = app.listen(done)
+  request = supertest.agent(server)
 })
 
-after((done)=> {
+after((done) => {
   server.close(done)
 })
+
+beforeEach(async () => {
+  await factory.createMany('Book', 2, [
+    { id: 100, title: 'Learning NodeJS for dummies' },
+    { id: 900, title: 'Learning Angular for noobies' }
+  ])
+});
+
+afterEach(() => {
+  factory.cleanUp();
+});
 
 describe('GET /api/v1/books', () => {
   before(async () => {
@@ -25,24 +37,23 @@ describe('GET /api/v1/books', () => {
 
   it('responds with list of books', () => {
     expect(response.body.books)
-    .to.be.an("array")
+      .to.be.an("array")
   });
 
   it('returns title for book', () => {
-    expect(response.body.books[1].title).to.equal('Learning Angular for dummies')
+    expect(response.body.books[1].title).to.equal('Learning Angular for noobies')
   });
 })
 
 describe('GET /api/v1/books/:id', () => {
-  before(async () => {
-    response = await request.get('/api/v1/books/1')
-  })
 
-  it('responds with status 200', () => {
+  it('responds with status 200', async () => {
+    response = await request.get('/api/v1/books/100')
     expect(response.status).to.equal(200)
   });
 
-  it('responds a single book', () => {
-    expect(response.body.book.id).to.equal(1)
+  it('responds a single book', async () => {
+    response = await request.get('/api/v1/books/900')
+    expect(response.body.book.id).to.equal(900)
   });
 });
--- test_helpers/index.js
+++ test_helpers/index.js
@@ -2,13 +2,19 @@
 const factoryGirl = require('factory-girl');
 const adapter = new factoryGirl.SequelizeAdapter();
 const factory = factoryGirl.factory;
+
 factory.setAdapter(adapter);
+
 let Models = require('../models');
+
 // clean the factory state - necessary for mocha watch
+
 factory.cleanUp();
 factory.factories = [];
+
 // define factories
 require('./factories')(factory, Models);
+
 /* uncomment to see UnhandledPromiseRejectionWarning stack traces */
 /*
 process.on('unhandledRejection', (reason, p) => {
@@ -16,11 +22,13 @@ process.on('unhandledRejection', (reason, p) => {
   console.log('reason:', reason);
 });
 */
+
 beforeEach(done => {
   Models.sequelize.sync({ force: true }).then(() => {
     done();
   });
 });
+
 module.exports = {
   factory: factory,
   Models: Models
